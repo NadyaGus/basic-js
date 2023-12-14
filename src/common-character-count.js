ceed493105e87f09bf7cf0c418847ca13ac93457
NadyaGus <ftotkexit@gmail.com>
2023-12-14 15:28:13 +0300

    feat: 46 passing / 37 pending

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  let arr1 = s1.split("").sort();
+  let arr2 = s2.split("").sort();
+  for (let i = 0; i < s1.length; i += 1) {
+    if (arr1.includes(arr2[i])) {
+      arr1.shift();
+      count += 1;
+    } else {
+      arr1.shift();
+    }
+  }
+return count;
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,10 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function sortByHeight(arr) {
+  const sortedArr = [];
+  let sortedWithoutOne = arr.filter((i) => i > 0);
+  sortedWithoutOne.sort((a, b) => a - b);
+  let minIndex = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === -1) {
+      sortedArr.push(arr[i]);
+    } else {
+        sortedArr.push(sortedWithoutOne[minIndex]);
+        minIndex += 1;
+      }
+    }
+    return sortedArr;
+  }
 
 module.exports = {
   sortByHeight
